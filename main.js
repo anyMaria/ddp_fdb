44fd0c74be22104c1e111d8de3869a052acb7e95
anyMaria <anymaridim@gmail.com>
2026-04-30 15:01:09 +0200

    Add Cumbia Boom Box event details

--- main.js
+++ main.js
@@ -198,6 +198,16 @@ const programmationData = [
     categorie: "Spectacles & Théâtre",
     image: "/photos/diego-joanes.jpg",
     position: "center top"
+  },
+  {
+    nom: "Cumbia Boom Box",
+    jour: "05/07",
+    heure: "20h30 - 22h",
+    lieu: "Jardins des Isles",
+    description: "Cinq musiciennes, une sororité assumée et une énergie contagieuse. Entre rythmes acoustiques, voix enflammées et des beats électro, le groupe livre un live ultra-festif et taillé pour la danse. Rejoignez-nous pour clôturer la Fête des Berges avec une véritable explosion de bonne humeur et de puissance scénique.",
+    categorie: "Musique & Concerts",
+    image: "/photos/CUMBIA BOOM BOX PORTRAIT v2 nologo.jpg",
+    position: "center top"
   }
 ];
 
